e3759343c8308601ebf83c848d815094433b734a
Sawyer <sawyer.welter@gmail.com>
2024-03-13 19:45:31 -0500

    Reformatted file organization to move styling into separate css files

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,87 +1,88 @@
 import styled from 'styled-components'
+import '../styling/NasaPhoto.css'
 
-const StyledPhotoContent = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-    margin: 50px 10px;
-    padding:20px;
+// const StyledPhotoContent = styled.div`
+//     display: flex;
+//     flex-direction: row;
+//     justify-content: space-evenly;
+//     margin: 50px 10px;
+//     padding:20px;
 
-    img {
-        height: 70vh;
-        margin: 0 15px;
-        // max-width: 50%;
-    }
+//     img {
+//         height: 70vh;
+//         margin: 0 15px;
+//         // max-width: 50%;
+//     }
 
-    .text-wrapper {
-        background-color: lightgray;
-        width: 45%;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        height: 70vh;
-    }
+//     .text-wrapper {
+//         background-color: lightgray;
+//         width: 45%;
+//         display: flex;
+//         flex-direction: column;
+//         justify-content: center;
+//         height: 70vh;
+//     }
 
-    h1 {
-        font-size: 2rem;
-        padding: 10px;
-        margin: 10px;
-    }
+//     h1 {
+//         font-size: 2rem;
+//         padding: 10px;
+//         margin: 10px;
+//     }
 
-    p {
-        line-height: 1.3;
-        padding: 10px;
-        text-align: left;
-        margin: 20px;
+//     p {
+//         line-height: 1.3;
+//         padding: 10px;
+//         text-align: left;
+//         margin: 20px;
 
 
-        overflow-y: scroll;
+//         overflow-y: scroll;
 
-        ::-webkit-scrollbar {
-            width: 5px;
-        }
-        ::-webkit-scrollbar-track {
-            background: lightgray;
-        }
-        ::-webkit-scrollbar-thumb {
-            background: darkgray;
-        }
-        ::-webkit-scrollbar-thumb:hover {
-            background: black;
-        }
-    }
+//         ::-webkit-scrollbar {
+//             width: 5px;
+//         }
+//         ::-webkit-scrollbar-track {
+//             background: lightgray;
+//         }
+//         ::-webkit-scrollbar-thumb {
+//             background: darkgray;
+//         }
+//         ::-webkit-scrollbar-thumb:hover {
+//             background: black;
+//         }
+//     }
 
-    @media(max-width: 850px) {
-        flex-direction: column;
-        margin-top: 20px;
+//     @media(max-width: 850px) {
+//         flex-direction: column;
+//         margin-top: 20px;
 
-        .text-wrapper {
-            height: auto;
-            max-width: 100%;
-            width: 100%;
-            margin-top: 15px;
-        }
+//         .text-wrapper {
+//             height: auto;
+//             max-width: 100%;
+//             width: 100%;
+//             margin-top: 15px;
+//         }
 
-        img {
-            max-width: 100%;
-            width: 100%;
-            margin: 0;
-        }
-    }
-`;
+//         img {
+//             max-width: 100%;
+//             width: 100%;
+//             margin: 0;
+//         }
+//     }
+// `;
 
 
 
 function NasaPhoto(props) {
     return (
-        <StyledPhotoContent className="nasa-photo-wrapper">
+        <div className="nasa-photo-wrapper">
             <img src={props.photo.hdurl} />
             <div className="text-wrapper">
                 <h1>{props.photo.title}</h1>
                 <h3>{props.photo.copyright ? `by ${props.photo.copyright}` : ""} on {props.photo.date}</h3>
                 <p>{props.photo.explanation}</p>
             </div>
-        </StyledPhotoContent>
+        </div>
     )
 }
 
